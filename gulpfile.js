c84b55f889fac44dd8f18d0f77461c4cc0384810
Mark Coole <mark.coole@CraftedMedia.local>
2018-02-21 17:26:52 +0000

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+  return gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.reload({
+      stream: true
+  }))
+});
+
+gulp.task('watch', ['browserSync'], function(){
+  gulp.watch('scss/*.scss', ['sass']); 
+  // Other watchers
+  gulp.watch('*.html', browserSync.reload); 
+  gulp.watch('js/*.js', browserSync.reload);
+})
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
