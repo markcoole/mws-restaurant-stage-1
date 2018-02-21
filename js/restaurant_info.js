60eb4e991af8e6311b6a8e21ef686c5ec3304800
Mark Coole <mark.coole@btinternet.com>
2018-02-21 07:43:02 +0000

    Add classes to reviews

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -117,21 +117,36 @@ fillReviewsHTML = (reviews = self.restaurant.reviews) => {
  */
 createReviewHTML = (review) => {
   const li = document.createElement('li');
+  const div = document.createElement('div');
+  div.className = "name-date";
+
   const name = document.createElement('p');
+  name.className = "name";
   name.innerHTML = review.name;
-  li.appendChild(name);
+
+  //li.appendChild(name);
 
   const date = document.createElement('p');
+  date.className = "date";
   date.innerHTML = review.date;
-  li.appendChild(date);
+
+  div.appendChild(name)
+  div.appendChild(date);
+  li.appendChild(div);
+
+  const divp = document.createElement('div');
+  divp.className = "content";
 
   const rating = document.createElement('p');
+  rating.className = "rating";
   rating.innerHTML = `Rating: ${review.rating}`;
-  li.appendChild(rating);
+  li.appendChild(rating)
 
   const comments = document.createElement('p');
   comments.innerHTML = review.comments;
-  li.appendChild(comments);
+
+  divp.appendChild(comments);
+  li.appendChild(divp);
 
   return li;
 }
