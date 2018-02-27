955cea9c8d43f214aaa8d44ee3ad68973125c2aa
Mark Coole <mark.coole@btinternet.com>
2018-02-27 21:02:22 +0000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,43 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const imageResize = require('gulp-image-resize');
+const browserSync = require('browser-sync').create();
 
 gulp.task('sass', function(){
-  return gulp.src('scss/*.scss')
+  return gulp.src('_assets/scss/*.scss')
     .pipe(sass())
     .pipe(gulp.dest('css/'))
     .pipe(browserSync.reload({
       stream: true
   }))
+});
+
+gulp.task('images', () =>
+    gulp.src('_assets/img/*')
+        .pipe(imagemin([
+          imagemin.gifsicle({interlaced: true}),
+          imagemin.jpegtran({progressive: true}),
+          imagemin.optipng({optimizationLevel: 5}),
+          imagemin.svgo({
+              plugins: [
+                  {removeViewBox: true},
+                  {cleanupIDs: false}
+              ]
+          })
+      ], {verbose: true}))
+        .pipe(gulp.dest('img/'))
+);
+
+gulp.task('image-resize', function () {
+  gulp.src('_assets/img/*.jpg')
+    .pipe(imageResize({
+      width : 100,
+      height : 100,
+      crop : true,
+      upscale : false
+    }))
+    .pipe(gulp.dest('img/'));
 });
 
 gulp.task('watch', ['browserSync'], function(){
